b1ea778b6a7c17f6db82ad73a97c6098a037273b
Coodos <info@coodos.co>
2022-10-29 16:32:10 +0530

    refactor: remove useless interface file

--- src/identity-config.type.ts
+++
@@ -1,9 +0,0 @@
-import { DID, Document } from "@iota/identity-wasm/node";
-import { IStorageDriverProps } from "./StorageDriver/drivers/storage-driver.types";
-
-export type IdentityConfig = {
-  alias: string;
-  document: Document;
-  did: DID;
-  store: IStorageDriverProps;
-};
