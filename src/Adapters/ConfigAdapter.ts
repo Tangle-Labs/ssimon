ba9dd272bc10c1c014719b1e781374db3f4fbd2d
Coodos <info@coodos.co>
2023-01-08 17:37:09 +0530

    docs: `ConfigAdapter`

--- src/Adapters/ConfigAdapter.ts
+++ src/Adapters/ConfigAdapter.ts
@@ -13,12 +13,25 @@ export class ConfigAdapter {
     );
   }
 
+  /**
+   * Get an array of all the stored configs
+   *
+   * @returns Promise<IdentityConfig[]>
+   */
+
   async getIdentityConfig(): Promise<IdentityConfig[]> {
     const rawFileContent = await readFile(this.identityPath).catch((e) => {
       if (e.code === "ENOENT") return Buffer.from("[]");
     });
     return JSON.parse(rawFileContent.toString());
   }
+
+  /**
+   * Persist a new Identity to state
+   *
+   * @param {IdentityConfig} identity - the identity config to save
+   * @returns Promise<IdentityConfig>
+   */
 
   async saveIdentityConfig(identity: IdentityConfig): Promise<IdentityConfig> {
     const identities = await this.getIdentityConfig();
