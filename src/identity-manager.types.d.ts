faa1253a4f7a6b0d4e2c0830a943a431c0078ebc
Coodos <info@coodos.co>
2023-01-27 16:27:39 +0530

    chore: remove everything

--- src/identity-manager.types.d.ts
+++ src/identity-manager.types.d.ts
@@ -10,12 +10,6 @@ export type IdentityConfig = {
   store: IStorageDriverProps;
 };
 
-/**
- * Fragment type for IOTA Identity Fragments
- * example: #my-signing-method
- */
-export type Fragment = `#${string}`;
-
 export interface ICreateDidProps {
   alias: string;
   store: IStorageDriverProps;
