6ef02b96b5104eb1b49931019747803cdb1fd8fe
Merul Dhiman <69296233+coodos@users.noreply.github.com>
2023-03-08 16:30:16 +0530

    Feat/storage fix (#58)
    * fix: spec
    
    * chore: remove useless scripts

--- src/scripts/globalTeardown.ts
+++
@@ -1,6 +0,0 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-
-export = async function globalTeardown() {
-  const instance: MongoMemoryServer = (global as any).__MONGOINSTANCE;
-  await instance.stop();
-};
